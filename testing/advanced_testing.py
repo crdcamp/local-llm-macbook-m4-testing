519aeeab56b17a7e053cd7881ed168540aa410aa
Christian Camp <crdcamp@gmail.com>
2026-03-12 14:14:56 -0600

    Advanced testing setup

---
+++ testing/advanced_testing.py
@@ -0,0 +1,14 @@
+# %% Imports
+from pyexpat import model
+from llama_cpp import Llama
+import os
+import time
+
+# %% Define folder structure
+model_output_dir = "testing/model_outputs"
+os.makedirs(model_output_dir, exist_ok=True)
+
+# %% Figure out what metrics we need
+"""
+
+"""
