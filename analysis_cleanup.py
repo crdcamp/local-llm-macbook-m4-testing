0126711d5b14a09265420ca019be55c9b0d3ee79
Christian Camp <crdcamp@gmail.com>
2026-03-14 19:11:31 -0600

    Pivot tables done

--- analysis_cleanup.py
+++ analysis_cleanup.py
@@ -5,23 +5,35 @@ import matplotlib.pyplot as plt
 # %% Open (We'll get this in a better state later for file management)
 benchmarks_path = "benchmarks/benchmarks_20260313_155955.csv"
 benchmarks = pd.read_csv(benchmarks_path)
+print(benchmarks.columns)
 
 # Fix tokens per second calculation
 benchmarks['tokens_per_second'] = benchmarks["total_tokens"] / benchmarks['chat_completion_time']
 
 # Create "easy, medium", and "hard" prompt assignments
 # We'll move the prompts into a more centralized space another time
-# We'll also make sure to add a difficult parameter in the original function to avoid this mess
-def classify_difficulty(prompt):
-    if prompt.startswith("What is the capital"):
-        return "easy"
-    elif prompt.startswith("Summarize the main arguments"):
-        return "medium"
-    elif prompt.startswith("Compare the epistemological foundations of Bayesian"):
-        return "hard"
-    else:
-        return None
 
-benchmarks['prompt_difficulty'] = benchmarks['prompt'].apply(classify_difficulty)
+# %% Pivot Tables
+benchmarks_pivot_tokens = pd.pivot_table(benchmarks, index='prompt', columns='model', values='total_tokens', sort=True)
+benchmarks_pivot_time = pd.pivot(benchmarks, index='prompt', columns='model', values='chat_completion_time')
 
-print(benchmarks.head())
+
+# This is a sketchy way to plot but we'll fix that in the next project
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+
+benchmarks_pivot_tokens.plot(kind='bar', ax=ax1)
+ax1.set_xlabel('Prompt Difficulty')
+ax1.set_ylabel('Total Tokens')
+
+ax1.set_xticklabels(['Hard', 'Easy', 'Medium'])
+
+ax1.set_title('Prompt Difficulty vs. Total Tokens')
+
+benchmarks_pivot_time.plot(kind='bar', ax=ax2)
+ax2.set_xlabel('Prompt Difficulty')
+ax2.set_ylabel('Chat Completion Time')
+
+ax2.set_xticklabels(['Hard', 'Easy', 'Medium'])
+
+ax2.set_title('Prompt Difficulty vs. Chat Completion Time')
+plt.show()
