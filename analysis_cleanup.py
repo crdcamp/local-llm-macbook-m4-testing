8c6f8e315e55b10bfe2a8685b5558f6a1a74a4ef
Christian Camp <crdcamp@gmail.com>
2026-03-14 20:38:14 -0600

    More graphs yay

--- analysis_cleanup.py
+++ analysis_cleanup.py
@@ -13,28 +13,38 @@ benchmarks['tokens_per_second'] = benchmarks["total_tokens"] / benchmarks['chat_
 # %% Create pivot tables
 benchmarks_pivot_tokens = pd.pivot_table(benchmarks, index='prompt', columns='model', values='total_tokens', sort=True)
 benchmarks_pivot_time = pd.pivot(benchmarks, index='prompt', columns='model', values='chat_completion_time')
+benchmarks_pivot_tps = pd.pivot(benchmarks, index='prompt', columns='model', values='tokens_per_second')
 
+# Tokens/second pivot table
 
 # %% Graphing pivot tables
 # This is a sketchy way to plot but we'll fix that in the next project
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 6))
 
+# Prompt Difficulty vs. Tokens
 benchmarks_pivot_tokens.plot(kind='bar', ax=ax1)
 ax1.set_xlabel('Prompt Difficulty')
 ax1.set_ylabel('Total Tokens')
-
 ax1.set_xticklabels(['Hard', 'Easy', 'Medium'])
-
 ax1.set_title('Prompt Difficulty vs. Total Tokens')
 
+# Prompt Difficulty vs. Chat Completion Time
 benchmarks_pivot_time.plot(kind='bar', ax=ax2)
 ax2.set_xlabel('Prompt Difficulty')
 ax2.set_ylabel('Chat Completion Time')
-
 ax2.set_xticklabels(['Hard', 'Easy', 'Medium'])
-
 ax2.set_title('Prompt Difficulty vs. Chat Completion Time')
+
+# Prompt Difficulty vs. Tokens/Second
+benchmarks_pivot_tps.plot(kind='bar', ax=ax3)
+ax3.set_title('Prompt Difficulty vs. Tokens/Second')
+ax3.set_xlabel('Prompt Difficulty')
+ax3.set_ylabel('Tokens/Second')
+ax3.set_xticklabels(['Hard', 'Easy', 'Medium'])
+ax3.set_title('Prompt Difficulty vs. TPS')
+
 plt.show()
 
 # %% Now we should calculate ratios or something between these....
 # Let's look into interpreting pivot tables before continuing
+# Maybe calculate the token to chat completion time ratio in some way???? (That's literally tokens/second dummy)
