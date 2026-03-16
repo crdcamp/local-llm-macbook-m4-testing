cd738c7a302a8ddd4ec9ba74505d2601c78a96b2
Christian Camp <crdcamp@gmail.com>
2026-03-16 17:52:31 -0600

    Time to start using llama bench

--- analysis_cleanup.py
+++ analysis_cleanup.py
@@ -1,7 +1,6 @@
 # %% Imports
 import pandas as pd
 import matplotlib.pyplot as plt
-from pandas.core.reshape.pivot import pivot_table
 
 # %% Open (We'll get this in a better state later for file management)
 benchmarks_path = "benchmarks/benchmarks_20260313_155955.csv"
@@ -42,12 +41,12 @@ benchmarks_pivot_tps.plot(kind='bar', ax=ax3)
 ax3.set_title('Prompt Difficulty vs. Tokens/Second')
 ax3.set_xlabel('Prompt Difficulty')
 ax3.set_ylabel('Tokens/Second')
-ax3.set_xticklabels(['Hard', 'Easy', 'Medium'])
+ax3.set_xticklabels(['Hard', 'Medium', 'Easy'])
 ax3.set_title('Prompt Difficulty vs. TPS')
 
 plt.show()
 
-# %% Now we should calculate ratios or something between these....
+1# %% Now we should calculate ratios or something between these....
 # Let's look into interpreting pivot tables before continuing
 # Maybe calculate the token to chat completion time ratio in some way???? (That's literally tokens/second dummy)
 
@@ -57,15 +56,14 @@ plt.show()
 pivot_tables = {
     "tokens": pd.pivot_table(benchmarks, index='prompt', columns='model', values='total_tokens', sort=True),
     "time": pd.pivot(benchmarks, index='prompt', columns='model', values='chat_completion_time'),
-    "tpd": pd.pivot(benchmarks, index='prompt', columns='model', values='tokens_per_second')
+    "tps": pd.pivot(benchmarks, index='prompt', columns='model', values='tokens_per_second')
 }
 
-print(pivot_tables['tokens'].columns)
 for key, value in pivot_tables.items():
-    pivot_tables[key] = pivot_tables[key].drop(['8B_qwen_3', '0.5B_ruvltra'], axis=1)
+    pivot_tables[key] = pivot_tables[key].drop(['8B_qwen_3', '0.5B_ruvltra'], axis=1) # These are bad data imo
 
 # Should really make a function for this
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 6));
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 6))
 
 pivot_tables['tokens'].plot(kind='bar', ax=ax1)
 ax1.set_xlabel('Prompt Difficulty')
@@ -82,11 +80,16 @@ ax2.set_title('Prompt Difficulty vs. Chat Completion Time')
 
 # Prompt Difficulty vs. Tokens/Second
 # (This graph might be useless)
-pivot_tables['tpd'].plot(kind='bar', ax=ax3)
+pivot_tables['tps'].plot(kind='bar', ax=ax3)
 ax3.set_title('Prompt Difficulty vs. Tokens/Second')
 ax3.set_xlabel('Prompt Difficulty')
 ax3.set_ylabel('Tokens/Second')
-ax3.set_xticklabels(['Hard', 'Easy', 'Medium'])
+ax3.set_xticklabels(['Hard', 'Medium', 'Easy'])
 ax3.set_title('Prompt Difficulty vs. TPS')
 
 plt.plot();
+
+"""
+Welp.... now I guess it's time to make a few shell scripts for llama bench to get some actual metrics
+instead of doing all of this by hand.
+"""
